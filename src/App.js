cd2d2845b7627a6a7154c8a84d5d25ec6c120b45
yujin779 <51077021+yujin779@users.noreply.github.com>
2020-12-28 00:19:20 +0900

    床を上下二段に増やした

--- src/App.js
+++ src/App.js
@@ -8,8 +8,34 @@ import { Physics, useBox, usePlane, useSphere } from "use-cannon";
 import styles from "./styles";
 import { createGlobalState } from "react-hooks-global-state";
 
-import Floor from "./components/Floor";
-import Player from "./components/Player";
+const DropBox = () => {
+  const [ref] = useBox(() => ({
+    mass: 1,
+    args: [1, 1, 1],
+    position: [0, 7, 0]
+  }));
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshStandardMaterial attach="material" color={"orange"} />
+    </mesh>
+  );
+};
+
+const Floor = ({ position, args }) => {
+  const [ref] = useBox(() => ({
+    type: "Static",
+    mass: 1,
+    args: args,
+    position: position
+  }));
+  return (
+    <mesh ref={ref}>
+      <boxBufferGeometry attach="geometry" args={args} />
+      <meshStandardMaterial attach="material" color={"hotpink"} />
+    </mesh>
+  );
+};
 
 /*
  * 1. 表示される入り口
@@ -18,16 +44,17 @@ const App = () => {
   return (
     <View style={styles.app}>
       <Canvas camera={{ position: [0, 2, 10], near: 0.1, far: 50 }}>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <ambientLight intensity={0.7} />
+        <spotLight position={[20, 20, 20]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
         <CameraController />
         <Physics
           gravity={[0, -30, 0]}
           defaultContactMaterial={{ restitution: 0.5 }}
         >
-          <Player />
-          <Floor />
+          <DropBox />
+          <Floor position={[0, 0, 0]} args={[5, 0.3, 5]} />
+          <Floor position={[0, -4, 0]} args={[15, 0.3, 15]} />
         </Physics>
       </Canvas>
     </View>
